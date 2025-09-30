c8d760b6d024d4205986074ab7fc76f88eda17e5
khaphong229 <khaphong217@gmail.com>
2025-09-30 11:03:05 +0700

    feat: response interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from 'src/common/http-exception.filter';
+import { TransformResponseInterceptor } from 'src/common/transform-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,7 @@ async function bootstrap() {
   );
 
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalInterceptors(new TransformResponseInterceptor());
 
   await app.listen(process.env.PORT ?? 3000);
 }
